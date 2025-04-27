4f6c554d83051e5807bcbb33bfb9020a5de50ace
fabri-123 <fabricioenavarro@gmail.com>
2025-04-27 19:17:39 -0300

    subo mi repositorio del tp

---
+++ app.js
@@ -0,0 +1,7 @@
+let nombre = "Juan";
+let edad = 31;
+let ciudad = "Banda Del Rio Sali";
+let comidaFavorita = " ñoquis";
+let eresFeliz = true;
+
+console.log("hola mi nombre es :",  nombre + " y tengo ", edad + " años y vivo en ", ciudad + " y mi comida favorita es el ", comidaFavorita + " ,  y si me preguntas si soy muy feliz las respuesta es :", eresFeliz + " , pues Dios me a bendecido mucho 😁 🙌");
