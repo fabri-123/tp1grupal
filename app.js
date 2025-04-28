58162b8bacc4ea3b860163894fc183534dcd812f
fabri-123 <fabricioenavarro@gmail.com>
2025-04-27 22:35:26 -0300

    agrego cambios al archivo app.js

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ let comidaFavorita = " ñoquis";
 let eresFeliz = true;
 
 console.log("hola mi nombre es :",  nombre + " y tengo ", edad + " años y vivo en ", ciudad + " y mi comida favorita es el ", comidaFavorita + " ,  y si me preguntas si soy muy feliz las respuesta es :", eresFeliz + " , pues Dios me a bendecido mucho 😁 🙌");
+
+let apellido = "Gonzalez";
+console.log("mi apellido es :", apellido);
